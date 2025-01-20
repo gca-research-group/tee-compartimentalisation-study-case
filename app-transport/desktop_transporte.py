1deab57ea4bd85dc48e5d9584e6a447f5146aeb1
regis-schuch <43655270+regis-schuch@users.noreply.github.com>
2025-01-20 14:16:58 -0300

    Update desktop_transporte.py

--- app-transport/desktop_transporte.py
+++ app-transport/desktop_transporte.py
@@ -1,9 +1,4 @@
 ##
-# Programmer   : Regis Rodolfo Schuch
-# Date         : 10 June 2024
-#              : Applied Computing Research Group, Unijui University, Brazil
-#              : regis.schuch@unijui.edu.br
-#              :
 # Title        : desktop_transporte.py 
 #              :
 # Description  : The desktop_transporte.py code defines a GUI application using Tkinter to schedule transport 
