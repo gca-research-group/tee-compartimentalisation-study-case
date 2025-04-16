f752646ea40871bc0082f56e464cd6f744b4591e
regis-schuch <43655270+regis-schuch@users.noreply.github.com>
2025-04-16 10:53:55 -0300

    Update exchangekeysclient.py

--- overhead-analysis/exchange-keys/client/exchangekeysclient.py
+++ overhead-analysis/exchange-keys/client/exchangekeysclient.py
@@ -8,20 +8,16 @@ MY_ID = "integration_process"
 MY_PUBLIC_KEY_FILE = "public_key.pem"
 CSV_FILE = "exchangeKeys_results.csv"
 
-# Carregar chave pública
 with open(MY_PUBLIC_KEY_FILE, "r") as f:
     pubkey_pem = f.read()
 
-# Payload base
 payload = {
     "id": MY_ID,
     "public_key": pubkey_pem
 }
 
-# Desabilitar TLS apenas para testes locais
 verify_tls = False
 
-# Criar arquivo CSV
 with open(CSV_FILE, mode="w", newline="") as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(["iteration", "duration_ms", "status"])
